a6a937d35599c5f41c032a0ae18614277da5f8e2
Kuroshinibalakumar <kuroshinibalakumar050@gmail.com>
2024-06-12 21:52:41 +0530

    Add files via upload

---
+++ pro.js
@@ -0,0 +1,20 @@
+<script>
+  //Js
+ 
+	document.querySelector(".mobile-btn").addEventListener("click", function () {
+	document.querySelector(".menu").classList.toggle("active");
+}); 
+
+
+</script>
+ <!-- Ion Icons Js -->
+    <script
+      type="module"
+      src="https://cdn.jsdelivr.net/npm/@ionic/core/dist/ionic/ionic.esm.js"
+    ></script>
+    <script
+      nomodule
+      src="https://cdn.jsdelivr.net/npm/@ionic/core/dist/ionic/ionic.js"
+    ></script>
+    <!-- JS -->
+    <script src="js/script.js"></script>
